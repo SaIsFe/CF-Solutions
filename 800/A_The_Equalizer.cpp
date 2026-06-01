0d51131eb23d8e12ea469342e707f195867d71d7
shanta_islam <shanta20154@gmail.com>
2026-06-01 05:42:40 -0700

    Initial commit

--- 800/A_The_Equalizer.cpp
+++ 800/A_The_Equalizer.cpp
@@ -11,15 +11,21 @@ f(i,n)cin>>v[i];
 #define no cout<<"NO\n"
 #define ll long long
 #define ff first
-#define ss second                                                                                                                                                                                         
+#define ss second
 int main() {
     optimize();
     int t;
     cin>>t;
     while(t--){
-int n;
-cin>>n;
-for(int i=2*n;i>n;i--)cout<<i<<' ';
-cout<<endl;
+   int n,k;
+   cin>>n>>k;
+   ll sum=0;
+   vector<int>v(n);
+   for(int i=0;i<n;i++){
+    cin>>v[i];
+    sum+=v[i];
     }
+    if(sum%2!=0 || (n*k)%2==0)yes;
+    else no;
+}
 }
