ac2d3e4295b3b18f4ab4aae35579ed4e85b3fa6c
shanta_islam <shanta20154@gmail.com>
2026-06-21 09:58:39 -0700

    Initial commit

--- 800/A_Find_K_Distinct_Points_with_Fixed_Center.cpp
+++ 800/A_Find_K_Distinct_Points_with_Fixed_Center.cpp
@@ -7,6 +7,8 @@ using namespace std;
 vector<long long>v(n);\
 f(i,n)cin>>v[i];
 #define pb push_back
+#define mxe(v) *max_element(v.begin(),v.end())
+#define mne(v) *min_element(v.begin(),v.end())
 #define yes cout<<"YES\n"
 #define no cout<<"NO\n"
 #define ll long long
@@ -17,25 +19,15 @@ int main() {
     int t;
     cin>>t;
     while(t--){
-   int n;
-   cin>>n;
-   vector<ll>v(n);
-   for(int i=0;i<n;i++)cin>>v[i];
-   vector<ll>ans(n);
-   int j=0;
-   for(int i=0;i<(n+1)/2;i++){
-ans[j]=(v[i]);
-j=j+2;
-   }
-   //for(auto x:ans)cout<<x<<' ';
-   //cout<<endl;
-   int index=(n+1)/2;
-   int k=1;
-   for(int i=n-1;i>=index;i--){
-    ans[k]=v[i];
-    k=k+2;
-   }
-   for(auto x:ans)cout<<x<<' ';
-   cout<<endl;
+  int x,y,k;
+  cin>>x>>y>>k;
+  if(k%2!=0){
+    cout<<x<<' '<<y<<endl;
+    k=k-1;
+  }
+  for(int i=1;i<=k/2;i++){
+    cout<<x-i<<' '<<y<<endl;
+    cout<<x+i<<' '<<y<<endl;
+  }
     }
 }
