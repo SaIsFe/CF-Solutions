35d23df36e738005219b9666ea14d380b4bc524f
shanta_islam <shanta20154@gmail.com>
2026-06-22 09:49:14 -0700

    Initial commit

---
+++ 1000/B_Minimize_Equal_Sum_Subarrays.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define f(i,n)for(int i=0;i<n;i++)
+#define iv(v,n) \
+vector<long long>v(n);\
+f(i,n)cin>>v[i];
+#define pb push_back
+#define mxe(v) *max_element(v.begin(),v.end())
+#define mne(v) *min_element(v.begin(),v.end())
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define ll long long
+#define ff first
+#define ss second
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+  int n;
+  cin>>n;
+  vector<int>v(n),ans;
+  for(int i=0;i<n;i++)cin>>v[i];
+for(int i=1;i<n;i++){
+    ans.pb(v[i]);
+}
+ans.pb(v[0]);
+for(auto x:ans)cout<<x<<' ';
+cout<<endl;
+    }
+}
