27bb7b4fe2b84258981123424ef077e6ccd58cdc
shanta_islam <shanta20154@gmail.com>
2026-06-29 01:13:49 -0700

    Initial commit

---
+++ Contest/Div 2/Round 1106/B_Crimson_Triples.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define f(i,n)for(int i=0;i<n;i++)
+#define iv(v,n) \
+vector<long long>v(n);\
+f(i,n)cin>>v[i];
+#define pb push_back
+#define mxe(v) *max_element(v.begin(),v.end())
+#define mne(v) *min_element(v.begin(),v.end())
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define ll long long
+#define ff first
+#define ss second
+int main() {
+    optimize();
+    int t;
+    cin>>t;
+    while(t--){
+  ll n;
+  cin>>n;
+ll cost=0;
+ll k;
+for(int i=1;i<=n;i++){
+    k=n/i;
+    cost+=k*k;
+}
+cout<<cost<<endl;
+    }
+}
